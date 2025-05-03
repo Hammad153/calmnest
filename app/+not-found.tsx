d427f6addab5c58508cf3fa0bd03be25e557fd17
Hammad153 <hammadismail1546@gmail.com>
2025-05-03 17:29:29 +0100

    feat: Add reusable button components and emoji picker functionality
    - Implemented a Button component with primary theme support.
    - Created a CircleButton component for circular action buttons.
    - Developed an EmojiList component to display a list of emojis for selection.
    - Added an EmojiPicker modal to allow users to choose emojis.
    - Introduced an EmojiSticker component for draggable and scalable emoji stickers.
    - Created an IconButton component for icon-based actions.
    - Implemented an ImageViewer component to display images with optional URI support.

--- app/+not-found.tsx
+++ app/+not-found.tsx
@@ -1,32 +1,27 @@
-import { Link, Stack } from 'expo-router';
-import { StyleSheet } from 'react-native';
-
-import { ThemedText } from '@/components/ThemedText';
-import { ThemedView } from '@/components/ThemedView';
+import { View, StyleSheet } from "react-native";
+import { Stack, Link } from "expo-router";
 
 export default function NotFoundScreen() {
-  return (
-    <>
-      <Stack.Screen options={{ title: 'Oops!' }} />
-      <ThemedView style={styles.container}>
-        <ThemedText type="title">This screen doesn't exist.</ThemedText>
-        <Link href="/" style={styles.link}>
-          <ThemedText type="link">Go to home screen!</ThemedText>
-        </Link>
-      </ThemedView>
-    </>
-  );
+    return (
+        <>
+        <Stack.Screen options={{ title: "Oops! Not found"}} />
+         <View style={styles.container}>
+            <Link href="/" style={styles.button}>Go back to home screen</Link>
+         </View>
+        </>
+    )
 }
-
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center',
-    padding: 20,
-  },
-  link: {
-    marginTop: 15,
-    paddingVertical: 15,
-  },
+    container: {
+        flex: 1,
+        justifyContent: "center",
+        alignItems: "center",
+        backgroundColor: "#25292e",
+    },
+    button: {
+        fontSize: 20,
+        textDecorationLine: 'underline',
+        color: '#fff',
+    }
 });
+// This is the screen that will be shown when a user tries to access a route that doesn't exist.
