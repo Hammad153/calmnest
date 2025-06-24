f3afce71a244de049cfb6dfebe5970e5bf6719c0
HammadDev <ahmad.asynctechs@gmail.com>
2025-06-24 19:59:43 +0100

    init the app and upgrade expo and dependencies

--- app/(tabs)/about.tsx
+++ app/(tabs)/about.tsx
@@ -5,16 +5,7 @@ export default function AboutScreen() {
     return(
         <View style={styles.container}>
             <Text style={styles.text}>
-                Sticker Smash is a fun and interactive mobile app built with React Native
-                that lets you unleash your creativity with digital stickers! Whether you're
-                customizing photos, creating expressive messages, or just playing around with cool designs,
-                Sticker Smash makes it easy and enjoyable.
-            </Text>
-
-            <Text style={styles.text}>
-                With a sleek and intuitive interface, you can drag, drop, resize, and rotate stickers on any image,
-                or simply create sticker-packed visuals from scratch.
-                Perfect for social media lovers, meme creators, and anyone who enjoys adding a splash of personality to their pictures.
+                About Calmnest
             </Text>
         </View>
     )
